833b0b3eba6632ba4566899bb19a87a0ea279332
smeetsfrank <franksmeets89@gmail.com>
2021-06-16 09:46:23 +0200

    update store with teams and update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import classes from './App.module.scss';
 
+import { useAppDispatch, useAppSelector } from './hooks/storeHooks';
+import { setTeams } from './store/slices/teamSlice';
+
+import exampleTeams from './api/football-data/example-teams';
+
+import Navigation from './components/Navigation/Navigation';
+import Matches from './components/Matches/Matches';
 import Standings from './components/Standings/Standings';
 
-const App: React.FC = () => (
-  <div className={classes.wrapper}>
-    <Standings />
-  </div>
-);
+const App: React.FC = () => {
+  const teams = useAppSelector((state) => state.teams);
+  const dispatch = useAppDispatch();
+
+  const fetchTeams = async () => {
+    const response = await exampleTeams();
+    const formattedTeams = response.teams.map((team) => ({
+      team: {
+        id: team.id,
+        name: team.name,
+        crestUrl: team.crestUrl,
+      },
+    }));
+    dispatch(setTeams(formattedTeams));
+  };
+
+  useEffect(() => {
+    fetchTeams();
+  }, []);
+
+  return (
+    <>
+      <Navigation />
+      <div className={classes.wrapper}>
+        <Switch>
+          <Route path="/matches">
+            <Matches />
+          </Route>
+          <Route path="/standings">
+            <Standings />
+          </Route>
+        </Switch>
+      </div>
+    </>
+  );
+};
 
 export default App;
