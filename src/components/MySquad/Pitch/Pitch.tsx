7c201a62a6e4327cc505d755d68711c2cf8fd471
smeetsfrank <franksmeets89@gmail.com>
2021-06-17 11:08:51 +0200

    add pitch styling layout

--- src/components/MySquad/Pitch/Pitch.tsx
+++ src/components/MySquad/Pitch/Pitch.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import classes from './Pitch.module.scss';
 
-const Pitch: React.FC = () => <div className={classes.pitch} />;
+const Pitch: React.FC = () => (
+  <div className={classes.pitch}>
+    <div className={`${classes['penalty-area']} ${classes.away}`} />
+    <div className={classes.center} />
+    <div className={`${classes['penalty-area']}`} />
+  </div>
+);
 
 export default Pitch;
